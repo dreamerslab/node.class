f8fd87468bd2d5489a87b157febb56b0691f8455
ben <ben@dreamerslab.com>
2013-12-16 17:27:42 -0800

    update test

--- test/run.js
+++ test/run.js
@@ -38,6 +38,22 @@ var what = {
 var Klass    = Class.inject( mixin_a, mixin_b ).extend( what );
 var instance = new Klass();
 
+var KKlas = Klass.inject({
+  x : '1'
+}, {
+  y : 2
+}).extend({
+  z : function (){
+    return '5';
+  }
+});
+
+Klass.should.have.property( 'inject' );
+Klass.should.have.property( 'extend' );
+
+KKlas.should.have.property( 'inject' );
+KKlas.should.have.property( 'extend' );
+
 instance.b.should.eql( 'whatever' );
 instance.d.should.eql( 'd' );
 instance.do_a().should.eql( 'awhatever' );
